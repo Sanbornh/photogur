45d036b8dca99164f8d9d04d2959c509dc2f61af
Sanborn <sanbornh@rogers.com>
2014-04-08 19:22:29 -0400

    Refactoring routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Photogur::Application.routes.draw do
 
+  resources :pictures
   root :to => 'pictures#index'
-  get  'pictures' => 'pictures#index'
+
+
+  # get  'pictures' => 'pictures#index'
   
-  post 'pictures' => 'pictures#create'
-  get  'pictures/new' => 'pictures#new'
+  # post 'pictures' => 'pictures#create'
+  # get  'pictures/new' => 'pictures#new'
 
-  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
-  patch 'pictures/:id' => "pictures#update"
+  # get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  # patch 'pictures/:id' => "pictures#update"
 
-  get  'pictures/:id' => 'pictures#show', as: "picture" 
+  # get  'pictures/:id' => 'pictures#show', as: "picture" 
 
-  delete 'pictures/:id' => 'pictures#destroy', as: 'delete_picture'
+  # delete 'pictures/:id' => 'pictures#destroy', as: 'delete_picture'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
